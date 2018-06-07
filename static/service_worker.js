6ae6601e482836081d540a6d42d3c11aff9420c3
RetroAstro <31494846+RetroAstro@users.noreply.github.com>
2018-06-07 11:29:39 +0800

    Update service_worker.js

--- static/service_worker.js
+++ static/service_worker.js
@@ -20,8 +20,8 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cache_static)
         .then( cache => cache.addAll(staticFiles))
+        .then(self.skipWaiting())
     )
-    return self.skipWaiting();
 })
 
 // 监听activate事件, 激活后通过cache的key来判断是否更新cache中的静态资源
@@ -33,8 +33,9 @@ self.addEventListener('activate', event => {
             }
         }))
     })
-    event.waitUntil(cachePromise);
-    return self.clients.claim();
+    event.waitUntil( 
+        cachePromise.then(self.clients.claim()) 
+    );
 })
 
 // 缓存策略 -- 在IOS 11.3.1 上有bug, 缓存后的音乐不能播放, 歌手图片不显示 --
