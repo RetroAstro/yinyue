a4ad876f0810c1bd00a176fc93bb095c31d786d3
RetroAstro <31494846+RetroAstro@users.noreply.github.com>
2018-06-07 16:07:21 +0800

    fix :bug:

--- static/service_worker.js
+++ static/service_worker.js
@@ -1,9 +1,12 @@
 
-var cache_static = 'static';
+var cacheMaps = {
+    cache_static: 'static_online',
+    cache_image: 'image_online',
+    cache_list: 'music_list',
+    last_playing_mid: 'last_playing_mid'
+}
 
-var cache_music = 'music';
-
-var cache_image = 'image';
+// var cache_music = 'music_online';
 
 var staticFiles = 
 [
@@ -18,24 +21,26 @@ var staticFiles =
 // 预缓存静态资源并立即激活 Service Worker
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(cache_static)
+        caches.open(cacheMaps.cache_static)
         .then( cache => cache.addAll(staticFiles))
-        .then(self.skipWaiting())
+        .then(() => self.skipWaiting())
     )
 })
 
 // 监听activate事件, 激活后通过cache的key来判断是否更新cache中的静态资源
 self.addEventListener('activate', event => {
-    var cachePromise = caches.keys().then( keys => {
-        return Promise.all( keys.map( key => {
-            if ( key !== cache_static ) {
+    event.waitUntil(
+        caches.keys()
+        .then( cacheNames => {
+            return cacheNames.filter( item => !Object.values(cacheMaps).includes(item))
+        })
+        .then( keys => {
+            return Promise.all( keys.map( key => {
                 return caches.delete(key);
-            }
-        }))
-    })
-    event.waitUntil( 
-        cachePromise.then(self.clients.claim()) 
-    );
+            }))
+        })
+        .then(() => self.clients.claim())
+    )
 })
 
 // 缓存策略 -- 在IOS 11.3.1 上有bug, 缓存后的音乐不能播放, 歌手图片不显示 --
@@ -48,7 +53,7 @@ self.addEventListener('fetch', event => {
     // 缓存图片
     else if ( /photo_new/.test(event.request.url) ) {
         var request = event.request;
-        event.respondWith(firstCache(request, cache_image));
+        event.respondWith(firstCache(request, cacheMaps.cache_image));
     }
     // 预缓存
     else if ( !/imgcache.qq.com/.test(event.request.url) ) {
@@ -64,11 +69,11 @@ function preCache(request) {
         if (cache) {
             return cache;
         }
-        return fetch(request);
+        return fetch(request.clone());
     })
     .catch( err => {
         console.log(err);
-        return fetch(request);
+        return fetch(request.clone());
     })
 }
 
@@ -79,11 +84,12 @@ function firstCache(request, cacheName) {
         if (cache) {
             return cache;
         }
-        return fetch(request)
+        var requestCopy = request.clone();
+        return fetch(requestCopy)
         .then( response => {
             var responseCopy = response.clone();
             caches.open(cacheName).then( cache => {
-                cache.put(request, responseCopy);
+                cache.put(requestCopy, responseCopy);
             })
             return response;
         })
